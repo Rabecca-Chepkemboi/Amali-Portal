9c85daf16158e7e517a3939f0185cbd2cbf6a822
chepkemboirabecca@gmail.com <chepkemboirabecca@gmail.com>
2023-11-07 11:46:08 +0300

    fetched movies in nextjs

--- src/app/hooks/useGetAthletes.tsx
+++ src/app/hooks/useGetAthletes.tsx
@@ -1,3 +1,41 @@
+// import { getAthletes } from "../utilities/utils";
+// import { useEffect, useState } from "react";
+
+// interface AthletesData {
+//   id: number;
+//   full_name: string;
+//   email: string;
+//   age: string;
+//   phone_number: string;
+//   profile_picture: string;
+//   achievements: string;
+//   password: string;
+//   role: string;
+// }
+
+
+// const useGetAthletes = () => {
+//   const [athletes, setAthletes] = useState<AthletesData[]>([]);
+
+
+//   useEffect(() => {
+//     const fetchAthletes = async () => {
+//         const fetchedAthletes = await getAthletes();
+//         setAthletes(fetchedAthletes);
+    
+//     };
+
+//     fetchAthletes();
+//   }, []);
+
+//   return { athletes };
+// };
+
+// export default useGetAthletes;
+
+
+
+
 import { getAthletes } from "../utilities/utils";
 import { useEffect, useState } from "react";
 
@@ -13,16 +51,13 @@ interface AthletesData {
   role: string;
 }
 
-
 const useGetAthletes = () => {
   const [athletes, setAthletes] = useState<AthletesData[]>([]);
 
-
   useEffect(() => {
     const fetchAthletes = async () => {
-        const fetchedAthletes = await getAthletes();
-        setAthletes(fetchedAthletes);
-    
+      const fetchedAthletes = await getAthletes();
+      setAthletes(fetchedAthletes);
     };
 
     fetchAthletes();
@@ -32,4 +67,3 @@ const useGetAthletes = () => {
 };
 
 export default useGetAthletes;
-
