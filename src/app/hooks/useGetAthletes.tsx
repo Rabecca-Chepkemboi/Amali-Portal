ab238280fdf7af33e7c0a301fd08957cd9a5fddc
chepkemboirabecca@gmail.com <chepkemboirabecca@gmail.com>
2023-11-08 09:31:16 +0300

    intergrated apis for registration

--- src/app/hooks/useGetAthletes.tsx
+++ src/app/hooks/useGetAthletes.tsx
@@ -33,37 +33,3 @@ const useGetAthletes = () => {
 
 export default useGetAthletes;
 
-
-
-
-// import { getAthletes } from "../utilities/utils";
-// import { useEffect, useState } from "react";
-
-// interface AthletesData {
-//   id: number;
-//   full_name: string;
-//   email: string;
-//   age: string;
-//   phone_number: string;
-//   profile_picture: string;
-//   achievements: string;
-//   password: string;
-//   role: string;
-// }
-
-// const useGetAthletes = () => {
-//   const [athletes, setAthletes] = useState<AthletesData[]>([]);
-
-//   useEffect(() => {
-//     const fetchAthletes = async () => {
-//       const fetchedAthletes = await getAthletes();
-//       setAthletes(fetchedAthletes);
-//     };
-
-//     fetchAthletes();
-//   }, []);
-
-//   return { athletes };
-// };
-
-// export default useGetAthletes;
